ee4baf0ffae37fcd79ecbd500191744e76b3cc6e
mrcattos <mrcatto69@gmail.com>
2023-06-06 22:20:22 +0200

    Prompt required

--- commands/GPT/Chat-gpt.js
+++ commands/GPT/Chat-gpt.js
@@ -10,11 +10,14 @@ const openai = new OpenAIApi(configuration);
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('gpt')
-        .setDescription('Ask ChatGPT a question'),
-
+        .setDescription('Ask ChatGPT a question')
+        .addStringOption(option =>
+            option.setName('prompt')
+                .setDescription('Your question')
+                .setRequired(true)),
     async execute(interaction) {
         try {
-            const prompt = interaction.toString(); // Convert to a string
+            const prompt = interaction.toString(); // Convert to a string. Will get a "BigInt" error otherwise
             const response = await openai.createCompletion({
                 model: "gpt-3.5-turbo", //defines model as gpt-3.5-turbo, open ai's latest released gpt model as of 5/6/2023
                 prompt: prompt,
