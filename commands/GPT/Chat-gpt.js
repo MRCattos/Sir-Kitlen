5ac49fe3836bc67f9831d13753c05e1f31811ced
mrcattos <mrcatto69@gmail.com>
2023-06-06 23:27:36 +0200

    error handling + prompt option which is required

--- commands/GPT/Chat-gpt.js
+++ commands/GPT/Chat-gpt.js
@@ -1,34 +1,51 @@
 const { SlashCommandBuilder } = require('discord.js');
-const { Configuration, OpenAIApi } = require("openai");
+const { Configuration, OpenAIApi } = require('openai');
 require('dotenv').config();
 
 const configuration = new Configuration({
-    apiKey: process.env.GPTOKEN,
+  apiKey: process.env.GPTOKEN,
 });
 const openai = new OpenAIApi(configuration);
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('gpt')
-        .setDescription('Ask ChatGPT a question')
-        .addStringOption(option =>
-            option.setName('prompt')
-                .setDescription('Your question')
-                .setRequired(true)),
-    async execute(interaction) {
-        try {
-            const prompt = interaction.toString(); // Convert to a string. Will get a "BigInt" error otherwise
-            const response = await openai.createCompletion({
-                model: "gpt-3.5-turbo", //defines model as gpt-3.5-turbo, open ai's latest released gpt model as of 5/6/2023
-                prompt: prompt,
-            });
+  data: new SlashCommandBuilder()
+    .setName('gpt')
+    .setDescription('Ask ChatGPT a question')
+    .addStringOption((option) =>
+      option
+        .setName('prompt')
+        .setDescription('Your question')
+        .setRequired(true)
+    ),
+  async execute(interaction) {
+    try {
+      const prompt = interaction.options.getString('prompt');
+      if (!prompt) {
+        throw new Error('No prompt provided.');
+      }
 
-            const Return = response.data.choices[0].message;
-            interaction.reply(Return);
+      const response = await openai.createCompletion({
+        model: 'gpt-3.5-turbo',
+        prompt: prompt,
+      });
 
-        } catch (error) {
-            console.error(error);
-            interaction.reply('Correct error handling has not been implemented for this command yet. Check the output terminal for the details.');
-        }
+      if (!response || !response.data || !response.data.choices || response.data.choices.length === 0) {
+        throw new Error('Invalid response received from OpenAI API.');
+      }
+
+      const returnMessage = response.data.choices[0].message.content;
+      await interaction.reply(returnMessage);
+    } catch (error) {
+      console.error('An error occurred:', error);
+      let errorMessage = 'An error occurred while processing your request.';
+
+      if (error.response && error.response.data && error.response.data.error) {
+        errorMessage = error.response.data.error.message;
+      } else if (error.message) {
+        errorMessage = error.message;
+      }
+
+      await interaction.reply(`Error message: ${errorMessage}`);
     }
+  },
 };
